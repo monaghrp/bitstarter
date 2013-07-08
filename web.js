31012134331b3c5fc8075f30dc41f8ea3078fdf5
Ryan Monaghan <ryan_p_monaghan@hotmail.com>
2013-07-08 04:11:19 +0000

    updated web.js

--- web.js
+++ web.js
@@ -2,9 +2,10 @@ var express = require('express');
 
 var fbuffer = fs.readFileSync("index.html");
 
-var str_buffer = fbuffer.toString();
+var str_buffer = fbuffer.toString('utf8',0, fbuffer);
 
 var app = express.createServer(express.logger());
+
 
 app.get('/', function(request, response) {
   response.send(str_buffer);
@@ -14,4 +15,3 @@ var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
-
