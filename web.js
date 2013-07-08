6c572fbf58b660067b674f23a4a362598fd88841
Ryan Monaghan <ryan_p_monaghan@hotmail.com>
2013-07-08 03:41:29 +0000

    updated web.js

--- web.js
+++ web.js
@@ -1,12 +1,17 @@
 var express = require('express');
 
+var buffer = fs.readFileSync("index.html");
+
+var str_buffer = buffer.toString();
+
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(str_buffer);
 });
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
+
